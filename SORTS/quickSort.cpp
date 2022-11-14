63ac1ca626b74f003aee5ebe8239f68f0f42e183
rohit <ramteker284@gmail.com>
2022-11-15 00:42:51 +0530

    Quick Sort

--- SORTS/quickSort.cpp
+++ SORTS/quickSort.cpp
@@ -13,22 +13,42 @@ void print(int arr[], int size) {
          cout << arr[i] << ' ';
 }
 
-int partitionIndex(int arr[], int n) {
-    
+int partitionIndex(int arr[], int low, int high, int n) {
+    int i = low;
+    int j = high;
+    int pivot = low;
+    while (j > i)
+    {
+        while(arr[pivot] >= arr[i]) i++;
+        while(arr[pivot] < arr[j]) j--;
+
+        if(j > i)
+            swap(arr[i], arr[j]);
+    }
+    swap(arr[pivot], arr[j]);
+
+    return j;
 }
 
-void quickSort(int arr[], int n) {
-    int partition = partitionIndex(arr, n);
+void quickSort(int arr[], int low, int high, int n) {
 
-
+    if( high < low ) return;
+    int partition = partitionIndex(arr, low, high, n);
+    quickSort(arr, low, partition-1, n);
+    quickSort(arr, partition+1, high, n);
 }
 
 int main()
 {
-    int arr[] = {9, 4, 5, 6, 30, 1, 5};
+    int arr[] = {9, 4, 5, 5, 6, 1, 7};
+    // int arr[] = {4,6,2,5,7,9,1,3};
     // int arr[] = {1,2,3,4,5,6,7};
 
     int n = sizeof(arr) / sizeof(arr[0]);
+
+    // partitionIndex(arr, 0, n-1, n);
+    quickSort(arr, 0, n-1, n);
+    print(arr, n);
     
     return 0;
 }
