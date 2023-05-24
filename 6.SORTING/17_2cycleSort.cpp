9835d2b56a6873edb1023f2a0314e7696c0cbe75
rohit <ramteker284@gmail.com>
2023-05-24 20:44:47 +0530

    Trying countSort

--- 6.SORTING/17_2cycleSort.cpp
+++ 6.SORTING/17_2cycleSort.cpp
@@ -1,30 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int arr[], int n) {
-    for (int i=0; i<n; i++)
-        cout << arr[i] << ' ';
-    cout << endl;
-}
+void countSort(int arr[], int n, int k) {
+    int count[k];
+    memset(count, 0, sizeof(count));
 
-void cycleSort(int arr[], int n) {
-    for (int i=0; i<n;) {
-        int correctIndex = arr[i] - 1;
-        if (arr[i] == arr[correctIndex]) 
-            i++;
-        else
-            swap(arr[i], arr[correctIndex]);
+    for (int i=0; i<n; i++) 
+        count[ arr[i] ]++;
+
+    // prefix sum
+    // 1 4 4 1 0 1
+    // 0 4 4 2 4
+    for (int i=1; i<n; i++)
+        count[i] = count[i-1] + count[i];
+
+    int output[n];
+
+    for(int i=n-1; i>=0; i--) {
+        int index = count[ arr[i] ];
+        output[ index - 1 ] = arr[i];
+        count[ arr[i] ]--;
+    }
+    for (int i=0; i<n; i++) 
+    {
+        arr[i] = output[i];
+        cout << arr[i] << ' ';
     }
 }
 
 int main()
 {
-    // arr is given in the range of 1 to n, now sort the arr in single pass
-    int arr[] = {5,1,3,2,4};
+    int arr[] = {1,4,4,1,0,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    cycleSort(arr,5);
+    int k = *max_element(arr, arr + n) + 1;
+    countSort(arr, n, k);
     
-    print(arr, 5);
-
     return 0;
 }
